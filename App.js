03a92a5628d523687042023e83063d8e2b0982ec
Thomas Hodges <thodges@javascriptonline.org>
2019-06-19 15:10:04 -0700

    react store set up

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import {View} from 'react-native';
+import React from 'react'
+import {View} from 'react-native'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import reducers from './src/reducers'
 
 const App = () => {
   return (
-    <View />
-  );
+    <Provider store={createStore(reducers)}>
+      <View />
+    </Provider>
+  )
 }
 
 export default App
